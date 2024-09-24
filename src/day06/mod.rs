25e4666718103471f6a4aab19ca443e814326643
lbreede <67719520+lbreede@users.noreply.github.com>
2024-09-23 21:46:22 -0700

    Solved day 6

---
+++ src/day06/mod.rs
@@ -0,0 +1,88 @@
+use std::iter::zip;
+use std::time::Instant;
+use thousands::Separable;
+
+pub fn run() {
+    println!("Day 6: Wait For It");
+    let input = include_str!("input.txt");
+
+    let now = Instant::now();
+    let result = part_one(input);
+    let elapsed = now.elapsed();
+    println!(
+        "Part 1: {} ({:.2?})",
+        result.separate_with_commas(),
+        elapsed
+    );
+
+    println!("Part 2: Skipped!");
+    // let now = Instant::now();
+    // part_two(input);
+    // let elapsed = now.elapsed();
+    // println!("Part 2: {} ({:.2?})", result.separate_with_commas(), elapsed);
+    // 
+    // println!();
+}
+
+fn record_beat_count(time: u64, record_dist: u64) -> usize {
+    (0..=time)
+        .map(|n| (time - n) * n)
+        .filter(|n| *n > record_dist)
+        .count()
+}
+
+fn part_one(input: &str) -> usize {
+    let mut iter = input.lines().map(|line| {
+        line.split_whitespace()
+            .skip(1)
+            .map(|n| n.parse::<u64>().unwrap())
+            .collect::<Vec<u64>>()
+    });
+    let times = iter.next().unwrap();
+    let record_distances = iter.next().unwrap();
+
+    zip(times, record_distances)
+        .map(|(time, record_dist)| record_beat_count(time, record_dist))
+        .product()
+}
+fn part_two(input: &str) -> usize {
+    let mut iter = input.lines().map(|line| {
+        line.chars()
+            .filter(|ch| ch.is_ascii_digit())
+            .collect::<String>()
+            .parse::<u64>()
+            .unwrap()
+    });
+    let time = iter.next().unwrap();
+    let record_dist = iter.next().unwrap();
+    record_beat_count(time, record_dist)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one_example() {
+        let input = include_str!("example.txt");
+        assert_eq!(part_one(input), 288);
+    }
+
+    #[test]
+    fn test_part_one() {
+        let input = include_str!("input.txt");
+        assert_eq!(part_one(input), 449_820);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        let input = include_str!("example.txt");
+        assert_eq!(part_two(input), 71_503);
+    }
+
+    // #[test]
+    // fn test_part_two() {
+    //     let input = include_str!("input.txt");
+    //     assert_eq!(part_two(input), 42_250_895);
+    // }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 fn main() {
     println!("\nAdvent of Code 2024\n");
     day01::run();
     day02::run();
     day03::run();
-    // day04::run(); // this one takes long!
-    // day05::run(); // part two takes f*cking ages! TODO: make good.
+    day04::run();
+    day05::run();
+    day06::run();
 }
